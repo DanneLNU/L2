3f5c344c4987a9885ddcb625091f747f9c494690
Daniel <da223dc@student.lnu.se>
2024-09-30 18:12:11 +0200

    finishes shaped clas

--- physics.js
+++ physics.js
@@ -94,7 +94,7 @@ class Rectangle extends Shape {
     }
 
     calculateInertia(mass) {
-        return (mass * (this.width ** 2 + this.height ** 2) / 12
+        return (mass * (this.width ** 2 + this.height ** 2) / 12)
     }
 
     containsPoint(point, position, angle = 0) {
@@ -110,3 +110,79 @@ class Rectangle extends Shape {
         )
     }
 }
+
+class Polygon extends Shape {
+    constructor(vertices) {
+        super('polygon')
+        this.vertices = vertices
+        this.calculateCentroid()
+    }
+
+    calculateCentroid() {
+        let centroid = new Vector2D(0, 0)
+        this.vertices.forEash((vertex) => {
+            centroid = centroid.add(vertex)
+        })
+        this.centroid = centroid.multiply(1 / this.vertices.length)
+    }
+
+    calculateInertia(mass) {
+        //placeholder for now...
+        //dont know yet how to calculate the inertia of a polygon
+        return mass
+    }
+
+    containsPoint(point, position, angle = 0) {
+        const localPoint = point.subtract(position).rotate(-angle)
+        let isInside = false
+        const n = this.vertices.length
+
+        for (let i = 0, j = n - 1; i < n; j = i++) {
+            const xi = this.vertices[i].x
+            const yi = this.vertices[i].y
+            const xj = this.vertices[j].x
+            const yj = this.vertices[j].y
+
+            const intersect =
+                yi > localPoint.y !== yj > localPoint.y &&
+                localPoint.x < ((xj - xi) * (localPoint.y - yi)) / (yj - yi + 0.0001) + xi
+            if (intersect) isInside = !isInside
+        }
+
+        return inside
+
+
+    }
+}
+
+
+/*=============================================
+    3. RigidBody Class
+    *========================================*/
+
+class RigidBody {
+    constructor(options) {
+        this.position = options.position || new Vector2D(0, 0)
+        this.velocity = options.velocity || new Vector2D(0, 0)
+        this.acceleration = new Vector2D(0, 0)
+        this.mass = options.mass || 1
+        this.invMass = this.mass !== 0 ? 1 / this.mass : 0
+        this.force = new Vector2D(0, 0)
+        this.shape = options.shape
+        this.restitution = options.restitution || 0.9
+        this.isStatic = options.isStatic || false
+        this.eventListeners = {}
+    }
+
+    applyForce(force) {
+        this.force = this.force.add(force)
+    }
+
+    integrate(deltaTime) {
+        if (this.isStatic) return
+
+        this.acceleration = this.force.multiply(this.invMass)
+        this.velocity = this.velocity.add(this.acceleration.multiply(deltaTime))
+        this.position = this.position.add(this.velocity.multiply(deltaTime))
+        this.force = new Vector2D(0, 0)
+    }
